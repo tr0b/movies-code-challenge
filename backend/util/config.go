00d7fdba4d7514332ce0e1884d19ddb890e53624
tr0b <trobinson3000600@gmail.com>
2024-05-27 00:22:54 -0600

    Will add configs to load .env vals

---
+++ backend/util/config.go
@@ -0,0 +1,33 @@
+package util
+
+import (
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+// Config will store all configurations of the application
+// These values will be read by viper, from a config file
+type Config struct {
+	DBDriver            string        `mapstructure:"DB_DRIVER"`
+	DBSource            string        `mapstructure:"DB_SOURCE"`
+	ServerAddress       string        `mapstructure:"SERVER_ADDRESS"`
+	TokenSymmetricKey   string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
+	AccessTokenDuration time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName(".env")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
